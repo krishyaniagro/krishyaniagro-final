5f9a92fe56e5c373404d74b43ca2eb460b3a2984
kiwikit <kiwisofttechwork@example.com>
2024-06-23 10:11:57 +0530

    added other changes

--- src/app/pages/contact/page.js
+++ src/app/pages/contact/page.js
@@ -1,108 +1,16 @@
 "use client"
-import Link from 'next/link';
-import { useLanguage } from '@/app/languagecontext';
+import ContactPage from "@/components/HomeComponents/ContactPage";
 
-const AboutUs = () => {
-    const { language } = useLanguage();
+const ContactUs = () => {
 
-    const translations = {
-        en: {
-            title: 'About Us',
-            companyOverview: 'Krushi Services is dedicated to providing premium quality fertilizers and agricultural solutions. With years of experience in the industry, we aim to enhance farm productivity and sustainability through innovative products and services.',
-            servicesOffered: 'Our services include:',
-            servicesList: [
-                'Organic and chemical fertilizers',
-                'Soil testing and analysis',
-                'Crop consultation and management',
-                'Sustainable farming practices',
-            ],
-            missionStatement: 'Our mission is to empower farmers with the tools and knowledge needed to achieve greater yields while preserving natural resources.',
-            contactUs: 'Contact Us',
-            address: '123 Main Street, Cityville, Country',
-            email: 'contact@example.com',
-            phone: '+1234567890',
-            visitUs: 'Visit Us',
-        },
-        mr: {
-            title: 'आमच्याबद्दल',
-            companyOverview: 'कृषी सेवांचा ध्येय उत्कृष्ट गुणवत्ताचे खते आणि कृषी समाधान प्रदान करणे आहे. उद्योगात वर्षांचा अनुभव असल्याने, आम्ही नवीनतम उत्पादने आणि सेवा प्रदान करून शेतीची उत्पादकता आणि सततता वाढवण्याच्या उद्दिष्टाने प्रयत्न करतो.',
-            servicesOffered: 'आमची सेवांमध्ये भरपूर आहेत:',
-            servicesList: [
-                'प्राकृतिक आणि रासायनिक खते',
-                'मृदाची चाचणी आणि विश्लेषण',
-                'फसलांची सल्ला आणि व्यवस्थापन',
-                'सतत शेती पद्धती',
-            ],
-            missionStatement: 'आमचे मिशन शेतकऱ्यांना महान पिके उत्पन्न करण्यासाठी आवश्यक उपकरणे आणि ज्ञान प्राप्त करण्याच्या मार्गाने अधिक यिल्ड्स मिळवणे आणि प्राकृतिक संसाधने संरक्षित करणे आहे.',
-            contactUs: 'आमच्याशी संपर्क साधा',
-            address: '१२३ मुख्य रोड, शहरगाव, देश',
-            email: 'contact@example.com',
-            phone: '+१२३४५६७८९०',
-            visitUs: 'आमच्या स्थानावर येऊन पहा',
-        },
-        hi: {
-            title: 'हमारे बारे में',
-            companyOverview: 'कृषि सेवाओं का ध्यान देना, उत्कृष्ट गुणवत्ता के खाद और कृषि समाधान प्रदान करना है। उद्योग में वर्षों के अनुभव के साथ, हम नवाचारी उत्पादों और सेवाओं के माध्यम से खेती की उत्पादकता और स्थिरता को बढ़ाने का लक्ष्य रखते हैं।',
-            servicesOffered: 'हमारी सेवाएं निम्नलिखित शामिल हैं:',
-            servicesList: [
-                'प्राकृतिक और रासायनिक खाद',
-                'मृदा परीक्षण और विश्लेषण',
-                'फसल सलाह और प्रबंधन',
-                'सतत खेती प्रथाएँ',
-            ],
-            missionStatement: 'हमारा मिशन किसानों को उन्नत उत्पादन प्राप्त करने के लिए जरूरी उपकरणों और ज्ञान प्रदान करना है जबकि प्राकृतिक संसाधनों को संरक्षित रखना है।',
-            contactUs: 'हमसे संपर्क करें',
-            address: '१२३ मुख्य सड़क, शहरगाँव, देश',
-            email: 'contact@example.com',
-            phone: '+1234567890',
-            visitUs: 'हमारे पास आएं',
-        },
-    };
 
     return (
-        <main className="container mx-auto pt-20 p-20">
-            <h1 className="text-5xl font-bold mb-8 text-center text-green-700">{translations[language].title}</h1>
+        <>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-                {/* Company Overview */}
-                <section className="flex flex-col items-center">
-                    <div>
-                        <h2 className="text-3xl font-semibold mb-4 text-green-600">Company Overview</h2>
-                        <p className="text-lg text-gray-700">{translations[language].companyOverview}</p>
-                    </div>
-                </section>
+            <ContactPage />
+        </>
 
-                {/* Services Offered */}
-                <section>
-                    <h2 className="text-3xl font-semibold mb-4 text-green-600">{translations[language].servicesOffered}</h2>
-                    <ul className="list-disc list-inside text-lg text-gray-700">
-                        {translations[language].servicesList.map((service, index) => (
-                            <li key={index} className="mb-2">{service}</li>
-                        ))}
-                    </ul>
-                </section>
-
-                {/* Mission Statement */}
-                <section>
-                    <h2 className="text-3xl font-semibold mb-4 text-green-600">Mission Statement</h2>
-                    <p className="text-lg text-gray-700">{translations[language].missionStatement}</p>
-                </section>
-
-                {/* Contact Us */}
-                <section>
-                    <div className="border p-6 rounded-lg shadow-lg bg-green-100">
-                        <h2 className="text-2xl font-semibold mb-4 text-green-600">{translations[language].contactUs}</h2>
-                        <p className="text-gray-700 mt-2"><strong>Address:</strong> {translations[language].address}</p>
-                        <p className="text-gray-700 mt-2"><strong>Email:</strong> {translations[language].email}</p>
-                        <p className="text-gray-700 mt-2"><strong>Phone:</strong> {translations[language].phone}</p>
-                        <Link href="/">
-                            <button className="bg-green-700 text-white mt-4 px-6 py-2 rounded-lg hover:bg-green-600">{translations[language].visitUs}</button>
-                        </Link>
-                    </div>
-                </section>
-            </div>
-        </main>
     );
 };
 
-export default AboutUs;
+export default ContactUs;
--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,14 +1,120 @@
-// Product.js
-"use client";
+"use client"
 import { useRouter } from 'next/navigation';
 import { useLanguage } from '@/app/languagecontext';
-import Cookies from 'js-cookie'; // Import js-cookie library
+import Cookies from 'js-cookie';
+import Image from 'next/image';
 
 const products = [
-  { id: 1, name: { en: 'Real Power Fertilizer', mr: 'रियल पावर खत', hi: 'रियल पावर उर्वरक' }, price: '$10.99', image: '/images/krushi_f1.jpg', description: { en: 'High-performance fertilizer for healthy plant growth.', mr: 'आरोग्यकर्ता पाऊस उभारण्यासाठी उच्च-क्रियाशील खत.', hi: 'स्वस्थ पौधे के विकास के लिए उच्च प्रदर्शन उर्वरक।' } },
-  { id: 2, name: { en: 'Sticky Fertilizer', mr: 'स्टिकी खत', hi: 'चिपचिपा उर्वरक' }, price: '$8.50', image: '/images/krushi_f2.jpg', description: { en: 'Adhesive fertilizer that improves nutrient absorption.', mr: 'संपीटनक खत ज्यामध्ये पोषण सोडविण्यासाठी सुधारले जाते.', hi: 'पोषक अवशोषण में सुधार करने वाला चिपचिपा उर्वरक।' } },
-  { id: 3, name: { en: 'Kepler Fertilizer', mr: 'केपलर खत', hi: 'केपलर उर्वरक' }, price: '$12.99', image: '/images/krushi_f3.jpg', description: { en: 'Revolutionary fertilizer with advanced growth formulas.', mr: 'प्रगत विकास सूत्रांसह क्रांतिकारी खत.', hi: 'उन्नत विकास सूत्रों वाला क्रांतिकारी उर्वरक।' } },
-  { id: 4, name: { en: 'Quality Change Fertilizer', mr: 'क्वालिटी चेंज खत', hi: 'क्वालिटी बदलें उर्वरक' }, price: '$9.99', image: '/images/krushi_f4.jpg', description: { en: 'Transformative fertilizer for improved soil health.', mr: 'माटीच्या आरोग्यासाठी सुधारित करण्यासाठी परिवर्तक खत.', hi: 'बेहतर मृदा स्वास्थ्य के लिए बदलावपूर्ण उर्वरक।' } },
+  {
+    id: 1,
+    name: { en: 'Real Power Fertilizer', mr: 'रियल पावर खत', hi: 'रियल पावर उर्वरक' },
+    usage: {
+      en: 'Ideal for all types of plants, including flowers, vegetables, fruits, and herbs.',
+      mr: 'फुले, भाज्या, फळे आणि ऑषधी च्या सर्व प्रकारांसाठी उपयुक्त आहे.',
+      hi: 'फूल, सब्जियां, फल और जड़ी-बूटियों सहित सभी प्रकार के पौधों के लिए आदर्श है।'
+    },
+    price: '₹ 10.99',
+    MRP: '₹ 20.99',
+    image: '/images/krushi_f1.jpg',
+    about: {
+      en: `
+        👑 𝐇𝐈𝐆𝐇-𝐐𝐔𝐀𝐋𝐈𝐓𝐘 <br /> Real Power Fertilizer   Enhances the overall growth of plants by providing essential nutrients. Stimulates the growth of roots, stems, and leaves, leading to healthier and more robust plants.
+      `,
+      mr: `
+        👑 𝐇𝐈𝐆𝐇-𝐐𝐔𝐀𝐋𝐈𝐓𝐘  <br /> उच्च गुणवत्ताचे असलेले रिअल पॉव्हर खत. आवश्यक खनिज प्रदान करून झाडांच्या समग्र वाढीला सुधारते. मूलवाहने, डोके आणि पानांची वाढी संवाद सुरू करते, अशी त्वरित वाढी करते, निरोगी आणि जोरदार झाडे होय.
+      `,
+      hi: `
+        👑 𝐇𝐈𝐆𝐇-𝐐𝐔𝐀𝐋𝐈𝐓𝐘 <br /> उच्च गुणवत्ता वाले रियल पावर उर्वरक। आवश्यक पोषक तत्व प्रदान करके पौधों की समग्र वृद्धि को बढ़ावा देता है। जड़ों, डंठली और पत्तों की वृद्धि को उत्तेजित करता है, जिससे पौधे स्वस्थ और मजबूत होते हैं।
+      `
+    },
+    ratings: {
+      average: '★ ★ ★ ★ ☆',
+      count: '(1,395 ratings)'
+    }
+  },
+  {
+    id: 2,
+    name: { en: 'Sticky Fertilizer', mr: 'स्टिकी खत', hi: 'चिपचिपा उर्वरक' },
+    usage: {
+      en: 'Adhesive fertilizer that improves nutrient absorption.',
+      mr: 'संपीटनक खत ज्यामध्ये पोषण सोडविण्यासाठी सुधारले जाते.',
+      hi: 'पोषक अवशोषण में सुधार करने वाला चिपचिपा उर्वरक।'
+    },
+    price: '₹ 8.50',
+    MRP: '₹ 15.99',
+    image: '/images/krushi_f2.jpg',
+    about: {
+      en: `
+        Sticky Fertilizer enhances nutrient absorption in plants, resulting in healthier growth.
+      `,
+      mr: `
+        स्टिकी खत रोगांची नियंत्रण करणारी, तज्ज्ञ संसाधनांच्या संग्रहाचे वापर करते.
+      `,
+      hi: `
+        चिपचिपा उर्वरक पौधों में पोषक अवशोषण में सुधार करता है, जिससे उनकी स्वस्थ वृद्धि होती है।
+      `
+    },
+    ratings: {
+      average: '★ ★ ★ ★ ☆',
+      count: '(1,200 ratings)'
+    }
+  },
+  {
+    id: 3,
+    name: { en: 'Kepler Fertilizer', mr: 'केपलर खत', hi: 'केपलर उर्वरक' },
+    usage: {
+      en: 'Revolutionary fertilizer with advanced growth formulas.',
+      mr: 'प्रगत विकास सूत्रांसह क्रांतिकारी खत.',
+      hi: 'उन्नत विकास सूत्रों वाला क्रांतिकारी उर्वरक।'
+    },
+    price: '₹ 12.99',
+    MRP: '₹ 24.99',
+    image: '/images/krushi_f3.jpg',
+    about: {
+      en: `
+        Kepler Fertilizer boosts plant growth significantly with its innovative formula.
+      `,
+      mr: `
+        केपलर खत त्याच्या आविष्कारी सूत्राच्या सहकार्याने रोगांची वृद्धी वाढविते.
+      `,
+      hi: `
+        केपलर उर्वरक अपने नवाचारी सूत्र से पौधों की वृद्धि में वृद्धि करता है।
+      `
+    },
+    ratings: {
+      average: '★ ★ ★ ★ ★',
+      count: '(2,500 ratings)'
+    }
+  },
+  {
+    id: 4,
+    name: { en: 'Quality Change Fertilizer', mr: 'क्वालिटी चेंज खत', hi: 'क्वालिटी बदलें उर्वरक' },
+    price: '₹ 9.99',
+    MRP: '₹ 18.99',
+    image: '/images/krushi_f4.jpg',
+    usage: {
+      en: 'Ideal for improving soil health and enhancing nutrient absorption.',
+      mr: 'माटीचे आरोग्य सुधारण्यासाठी आणि पोषक अवशोषण सुधारण्यासाठी उपयुक्त.',
+      hi: 'मृदा स्वास्थ्य में सुधार करने और पोषक अवशोषण को बढ़ाने के लिए आदर्श है।'
+    },
+    about: {
+      en: `
+        Transformative fertilizer for improved soil health. Quality Change Fertilizer enhances the nutrient content of the soil, ensuring healthier growth of plants.
+      `,
+      mr: `
+        माटीच्या आरोग्यासाठी सुधारित करण्यासाठी परिवर्तक खत. क्वालिटी बदलें उर्वरक माटीच्या पोषण सामग्रीचे सुधारणा करते, पौधोंच्या आरोग्याचे विकास सुनिश्चित करते.
+      `,
+      hi: `
+        बेहतर मृदा स्वास्थ्य के लिए बदलावपूर्ण उर्वरक। क्वालिटी बदलें उर्वरक मृदा के पोषक सामग्री को बढ़ावा देता है, जिससे पौधों की स्वस्थ वृद्धि होती है।
+      `
+    },
+    ratings: {
+      average: '★ ★ ★ ★ ☆',
+      count: '(1,200 ratings)' // Adjusted rating count based on the assumption
+    }
+  }
+  
+  
 ];
 
 const Product = ({ params }) => {
@@ -25,46 +131,67 @@ const Product = ({ params }) => {
     );
   }
 
-  const { name, price, image, description } = product;
+  const { name, price, image, usage, about ,MRP ,ratings} = product;
 
   const handleBuyNow = () => {
     const phoneNumber = '+918459574080';
     const productName = name[language];
-    const productDescription = description[language];
-    
-    // Check if user information exists in cookies
+    const productDescription = about[language];
+
     const username = Cookies.get('username');
     const address = Cookies.get('address');
     const userPhoneNumber = Cookies.get('user_phone_number');
 
     if (username && address && userPhoneNumber) {
-      const message = `my name is ${username}I want to buy ${productName} (${price}). Please confirm my order. Product details: ${productName} - ${productDescription}. My address is ${address}.`;
+      const message = `My name is ${username}. I want to buy ${productName} (${price}). Please confirm my order. Product details: ${productName} - ${productDescription}. My address is ${address}.`;
       const encodedMessage = encodeURIComponent(message);
       const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
 
       window.open(whatsappUrl, '_blank');
     } else {
       alert('Please provide your username, address, and phone number before purchasing.');
-      router.push('/auth/login')
+      router.push('/auth/login');
     }
   };
 
   return (
-    <main className="container mx-auto p-4 mt-16 h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg shadow-lg flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-8">
-        <div className="flex-shrink-0 w-full md:w-1/2">
-          <div className="w-full h-400px flex items-center justify-center">
-            <img src={image} alt={name[language]} className="max-w-full h-auto max-h-full object-contain rounded-lg" />
+    <div className="flex justify-center items-center min-h-screen pt-10">
+      <div className="bg-white p-6 rounded-lg max-w-4xl w-full">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 p-4">
+            <Image
+              src={image}
+              alt="Product Image"
+              width={500}
+              height={500}
+              className="rounded-lg"
+            />
+          </div>
+          <div className="md:w-1/2 p-4">
+            <h2 className="text-2xl font-bold mb-4">🌿 {name[language]} 🌿</h2>
+            <p className="text-gray-700 text-xl mb-4">
+              {usage[language]}
+            </p>
+            <div className="flex items-center mb-4">
+              <span className="text-yellow-500 mr-2">{ratings.average}</span>
+              <span className="text-gray-600">{ratings.count}</span>
+            </div>
+            <div className="text-xl font-bold text-green-500 mb-2">{price}</div>
+            <div className="text-sm text-gray-600 mb-4 line-through">M.R.P.: {MRP}</div>
+            <div className="mb-4">
+              <h2 className="text-2xl font-bold mb-4">Features :</h2>
+              <div dangerouslySetInnerHTML={{ __html: about[language] }} />
+            </div>
+            <div className="text-gray-600 mb-4">
+              <p>Fastest delivery Tomorrow, 23 June. Order within 18 hrs 4 mins.</p>
+            </div>
+            <div className="flex space-x-4">
+              <button onClick={handleBuyNow} className="bg-yellow-500 text-white py-2 px-16 rounded hover:bg-yellow-600">Buy Product</button>
+            </div>
           </div>
         </div>
-        <div className="flex-shrink-0 w-full md:w-1/2">
-          <h1 className="text-4xl font-bold mt-4 md:mt-0">{name[language]}</h1>
-          <p className="text-gray-700 mt-2">{price}</p>
-          <p className="mt-4">{description[language]}</p>
-          <button onClick={handleBuyNow} className="bg-green-500 text-white mt-4 p-2 rounded">Buy Now</button>
-        </div>
       </div>
-    </main>
+    </div>
   );
 };
 
