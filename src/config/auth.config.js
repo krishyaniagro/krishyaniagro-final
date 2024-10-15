b356776b7ea941312e312a0ca52dc1695d011d3c
kiwikit <kiwisofttechwork@gmail.com>
2024-10-15 14:23:21 +0530

    changed mongoUri

--- src/config/auth.config.js
+++ src/config/auth.config.js
@@ -1,4 +1,4 @@
 export const secret = process.env.ACCESS_TOKEN_SECRET || 'Dat@recovery@165';
 export const refreshSecret = process.env.REFRESH_TOKEN_SECRET || 'Dat@recovery@165';
-export const mongoURI = process.env.MONGO_URI || 'mongodb+srv://kiwisofttechwork:OrdCCqGPh9cXCrU7@websiste.3njnq.mongodb.net/data?retryWrites=true&w=majority&appName=websiste';
+export const mongoURI = process.env.MONGO_URI || 'mongodb+srv://krishyanisalesteam:2DvXkyqyeJeez3pi@krishyaniagro.piq0x.mongodb.net/data?retryWrites=true&w=majority&appName=krishyaniagro';
 export const port = process.env.PORT || 3000;
