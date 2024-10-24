392d40ff7f1490cc88063d8adca6d08a246efa2d
kiwikit <kiwisofttechwork@gmail.com>
2024-10-24 14:54:56 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -61,7 +61,7 @@ import Head from "next/head";
 export const metadata = {
   title: "Krishi Agro: Leading the Way in Agricultural Excellence",
   description: "Krishi Agro is dedicated to advancing sustainable farming through innovative solutions, cutting-edge technology, and expert support, empowering farmers to enhance productivity, efficiency, and environmental responsibility in modern agriculture.",
-  keywords:"sheti,gav,bajar,indian farming,khate,aushadhe,Krishyani agro,kissan,kissankesath,hamsathsath,agrytech,agriculture,agri,bharatseva,krushiseva,bharatkiseva,agricompany,ecomerce, agriinput,Farming techniques,Crop cultivation,Advanced farming equipment,Innovative agricultural practices,Indian farmers,Crop yield optimization,Farm management services,Sustainable agriculture,Precision farming,Soil fertility management,Pest control solutions,Crop protection products,Agri-inputs and fertilizers,Agricultural consultancy",
+  keywords:"sheti,gav,bajar,indian farming,khate,aushadhe,Krishyani agro,kissan,kissankesath,hamsathsath,agrytech,agriculture,agri,bharatseva,krushiseva,agricompany,ecomerce, agriinput,Farming techniques,Crop cultivation,Advanced farming equipment,Innovative agricultural practices,Indian farmers,Crop yield optimization,Farm management services,Sustainable agriculture,Precision farming,Soil fertility management,Pest control solutions,Crop protection products,Agri-inputs and fertilizers,Agricultural consultancy",
   verification: {
     google: "vvzb4VDojObD5cBKZ56tz36Cu0B7dIDVblkfv2S6COg",
   }
